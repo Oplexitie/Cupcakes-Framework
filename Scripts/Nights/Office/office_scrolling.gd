96b75709b47ed0a63ace0a1b4bf329a1ed9fea7b
Oplexitie <arthur.cottreau@gmail.com>
2024-05-23 21:52:02 +0200

    smooth scrolling + code cleanup

--- Scripts/Nights/Office/office_scrolling.gd
+++ Scripts/Nights/Office/office_scrolling.gd
@@ -2,19 +2,22 @@ extends Sprite2D
 
 class_name OfficeScroll
 
-@export var scroll_speed : float = 3
-@export var office_clamp : int = 650
+const SCROLL_SMOOTHING : int = 12 # Lower for smoother scrolling
+const SCROLL_SPEED : float = 0.07 # Lower for faster scrolling
+const SCROLL_CLAMP : int = 650 # Lower for faster scrolling
+
 # An export made for objects hitboxes that need an offset due to the equirectangular shader
 # Reminder that for each object there needs to be an Array of an Array to work
 # [NodePath, EdgeSpeed_x(int), SpeedClamp_left(int), SizeClamp_right(int)]
 @export var item_offsets : Array = []
 
 var scroll_area : Dictionary = { "Left": 0, "Right": 0 }
-var can_move = true
+var can_move : bool = true
+var scroll_amount : float = 0
 
 func _ready():
 	var view_size_x : float = get_viewport().content_scale_size.x
-	# Border correcter is made to fix a 1 pixel issue all the way to the right of the screen
+	# Made to fix a 1 pixel issue at the right of the screen when view_size_x isn't a whole number
 	var border_correcter_x : float = 1/(get_viewport().size.x/view_size_x)
 	
 	# This calculates the scroll area on both sides of the screen
@@ -25,21 +28,22 @@ func _ready():
 		i[0] = get_node(i[0])
 
 func _physics_process(delta):
-	var mouse_position : Vector2 = get_viewport().get_mouse_position()
-	var scroll_amount : float
+	var mouse_position : Vector2 = get_global_mouse_position()
 	
 	if can_move:
 		# Checks if the mouse is within one of the scroll areas, and scrolls if it is
 		if mouse_position.x < scroll_area["Left"]:
-			scroll_amount = (scroll_area["Left"] - mouse_position.x) * scroll_speed
+			scroll_amount += (scroll_area["Left"] - mouse_position.x) * SCROLL_SPEED
 		elif mouse_position.x > scroll_area["Right"]:
-			scroll_amount = (scroll_area["Right"] - mouse_position.x) * scroll_speed
+			scroll_amount += (scroll_area["Right"] - mouse_position.x) * SCROLL_SPEED
+	
+	scroll_amount = clamp(scroll_amount, -SCROLL_CLAMP, SCROLL_CLAMP)
 	
 	# Clamps the position so the office doesn't leave the frame
-	position.x = clamp(position.x + scroll_amount * delta, -office_clamp, office_clamp)
+	position.x = lerp(position.x, scroll_amount, SCROLL_SMOOTHING * delta)
 	apply_offset(scroll_amount, delta)
 
-func apply_offset(scroll_amount : float, delta : float):
+func apply_offset(scroll_modifier : float, delta : float):
 	# Modifies the buttons collision postion, makes it so the button is pressable with the shader
 	for i in item_offsets:
-		i[0].position.x = clamp((position.x - i[1] + i[0].position.x/2) + scroll_amount * delta, i[2], i[3])
+		i[0].position.x = clamp((position.x - i[1] + i[0].position.x/2) + scroll_modifier * delta, i[2], i[3])
