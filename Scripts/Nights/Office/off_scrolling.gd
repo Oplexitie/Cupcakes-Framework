3bfb62342b0329b2c852dbf02b5b738a170f926e
Oplexitie <arthur.cottreau@gmail.com>
2025-02-06 16:21:13 +0100

    Tweaks

--- Scripts/Nights/Office/off_scrolling.gd
+++ Scripts/Nights/Office/off_scrolling.gd
@@ -4,10 +4,10 @@ const SCROLL_SMOOTHING: int = 12 # Lower for smoother scrolling
 const SCROLL_SPEED: float = 0.07 # Lower for faster scrolling
 const SCROLL_CLAMP: int = 650 # Clamps office scrolling on both sides
 
-# An export made for objects hitboxes that need an offset due to the equirectangular shader
-# Reminder that for each object there needs to be an Array of an Array to work
-# [NodePath, EdgeSpeed_x(int), SpeedClamp_left(int), SizeClamp_right(int)]
-export var item_offsets: Array = []
+# An export made for trigger hitboxes that need an offset due to the equirectangular shader
+# Reminder that for each there needs to be an Array of an Array to work
+# [NodePath, XSpeed(int), LeftClamp(int), RightClamp(int)]
+export var trigger_offsets: Array = []
 
 var scroll_area_left: float
 var scroll_area_right: float
@@ -16,11 +16,11 @@ var can_move: bool = true
 
 func _ready() -> void:
 	_initialize_scroll_areas()
-	_initialize_items()
+	_initialize_triggers()
 
 func _physics_process(delta: float) -> void:
 	_handle_move(delta)
-	_apply_offset(scroll_amount, delta)
+	_handle_trigger_offsets(scroll_amount, delta)
 
 func _initialize_scroll_areas() -> void:
 	var view_size_x: float = get_viewport().size.x
@@ -31,15 +31,14 @@ func _initialize_scroll_areas() -> void:
 	scroll_area_left = view_size_x / 3
 	scroll_area_right = view_size_x - (scroll_area_left + border_correcter_x)
 
-func _initialize_items() -> void:
+func _initialize_triggers() -> void:
 	# Sets up object hitboxes
-	for i in item_offsets:
+	for i in trigger_offsets:
 		i[0] = get_node(i[0])
 
 func _handle_move(delta: float) -> void:
-	var mouse_position: Vector2 = get_global_mouse_position()
-	
 	if can_move:
+		var mouse_position: Vector2 = get_global_mouse_position()
 		# Checks if the mouse is within one of the scroll areas, and scrolls if it is
 		if mouse_position.x < scroll_area_left:
 			scroll_amount += (scroll_area_left - mouse_position.x) * SCROLL_SPEED
@@ -50,7 +49,7 @@ func _handle_move(delta: float) -> void:
 	scroll_amount = clamp(scroll_amount, -SCROLL_CLAMP, SCROLL_CLAMP)
 	position.x = lerp(position.x, scroll_amount, SCROLL_SMOOTHING * delta)
 
-func _apply_offset(scroll_modifier: float, delta: float) -> void:
+func _handle_trigger_offsets(scroll_modifier: float, delta: float) -> void:
 	# Modifies the buttons collision postion, makes it so the button is pressable with the shader
-	for i in item_offsets:
+	for i in trigger_offsets:
 		i[0].position.x = clamp((position.x - i[1] + i[0].position.x/2) + scroll_modifier * delta, i[2], i[3])
